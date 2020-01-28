fb5422f67240505b3e6492fdc7a6dc935d56fb24
Davi Miranda Gonçalves <davi.dmg@dpf.gov.br>
2020-01-28 13:30:40 -0200

    First commit

---
+++ src/main/java/com/dmgoncalves/umbackendqualquer/UmBackendQualquerApplication.java
@@ -0,0 +1,13 @@
+package com.dmgoncalves.umbackendqualquer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmBackendQualquerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmBackendQualquerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmgoncalves/umbackendqualquer/UmBackendQualquerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmgoncalves.umbackendqualquer;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class UmBackendQualquerApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
